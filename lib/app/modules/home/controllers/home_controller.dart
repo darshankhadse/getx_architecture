e98c2f6f9b28b1a861d7dbe22e8ed3067cba2d81
darshankhadse <darshan@batchsys.com>
2024-09-05 15:22:17 +0530

    Initial commit

---
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  //TODO: Implement HomeController
+
+  final count = 0.obs;
+  @override
+  void onInit() {
+    print('On Init');
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    print('On Ready');
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+}
